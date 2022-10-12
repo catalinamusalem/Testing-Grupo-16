ba3345ca0b3a124b83c593ef8039b531c85b394c
catalinamusalem <61999358+catalinamusalem@users.noreply.github.com>
2022-10-11 21:49:02 -0300

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'capybara/rails'
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 
 module ActiveSupport
